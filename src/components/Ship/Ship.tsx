5467c2da3a1a1f1d0c41cba055aa02d0ae453702
pozytron <michal.tymula@coderslab.pl>
2021-09-29 09:39:08 +0200

    Ship component is now displayed for each ship upon navigation

--- src/components/Ship/Ship.tsx
+++ src/components/Ship/Ship.tsx
@@ -1,7 +1,14 @@
-import React, {FC} from "react";
+import React from "react";
+import { RouteComponentProps } from 'react-router';
 
-const Ship:FC = ()=>{
-    return <h1>Ship</h1>
+
+type Props = { id: string };
+
+
+const Ship = ({match}: RouteComponentProps<Props>)=>{
+    const shipID = match.params.id
+    // fetch ship data for this layout or receive data from context or a props
+    return <h1>Ship with id: {shipID}</h1>
 }
 
 export default Ship;
