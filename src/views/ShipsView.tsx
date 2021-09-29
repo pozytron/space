5f5abee19ad0cf48729f0e5b3fdc50653cfb5326
pozytron <michal.tymula@coderslab.pl>
2021-09-29 08:59:51 +0200

    Ships view now shows "loading" while querying the data

--- src/views/ShipsView.tsx
+++ src/views/ShipsView.tsx
@@ -1,23 +1,43 @@
-import React, {FC, useEffect} from "react";
+import React, {FC, useEffect, useState} from "react";
 import SpaceXService from "../services/SpaceXService";
 import IShipData from "../types/IShipData";
 
 
 const ShipsView: FC = () => {
-    const [ships, setShips] = React.useState<Array<IShipData>>([]);
-    const [error, setError] = React.useState<string>("");
+    const [ships, setShips] = useState<Array<IShipData>>([]);
+    const [error, setError] = useState<string>("");
+    const [loading, setLoading] = useState<boolean>(true)
 
     useEffect(() => {
+        setLoading(true);
         SpaceXService.getShips()
-            .then(response => setShips(response.data))
+            .then(response => {
+                setShips(response.data);
+                setLoading(false)
+            })
             .catch(() => setError("Problem occurred while loading ship data, please try again"))
     }, [])
 
+    if(loading){
+        return (
+            <>
+                <h1>Ships</h1>
+                <p>Loading...</p>
+            </>
+        )
+    }
+    if(error){
+        return (
+            <>
+                <h1>Ships</h1>
+                <p>{error}</p>
+            </>
+        )
+    }
 
     return (
         <>
             <h1>Ships</h1>
-            {error!==""&&<p>{error}</p>}
             <ul>
                 {ships.map(ship=><li key={ship.id}>{ship.name}</li>)}
             </ul>
