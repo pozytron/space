b3d3852a510937dedf6d7394633645f75aa0cf3e
pozytron <michal.tymula@coderslab.pl>
2021-09-29 09:05:27 +0200

    endpoints for ships handled, added rockets for future development

--- src/services/SpaceXService.ts
+++ src/services/SpaceXService.ts
@@ -1,14 +1,21 @@
 import http from "../http-common";
 
-
-const getShips = ()=>{
+const getShips = () => {
     return http.get("/ships");
 };
 
+const getShip = (id: string) => {
+    return http.get(`/ships/${id}`);
+};
 
+const getRockets = () => {
+    return http.get("/rockets");
+}
 
 const SpaceXService = {
-getShips
+    getShips,
+    getShip,
+    getRockets
 };
 
 export default SpaceXService;
